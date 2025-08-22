f46731d5b1e0adaac1cfdd74e4e0dd1b2200a97f
devaspirant0510 <seungho020510@gmail.com>
2025-08-22 10:56:14 +0900

    fix: api 서버 연동

--- src/main/java/domain/book/user/service/BookService.java
+++ src/main/java/domain/book/user/service/BookService.java
@@ -12,8 +12,6 @@ import domain.book.user.dto.GenreCount;
 import domain.book.user.dto.PageList;
 import domain.book.user.repository.BookRepository;
 
-
-
 @Service
 public class BookService {
 	@Autowired
@@ -21,86 +19,83 @@ public class BookService {
 
 	@Autowired
 	BookRepository dao;
-	
+
 	public Book detail(Long id) {
 		return dao.findById(id);
 	}
-	
+
 	public List<GenreCount> getGenreCounts() {
-	    return dao.findGenreCounts();
+		return dao.findGenreCounts();
 	}
-	
+
 	public List<Book> getPopularBooks() {
-        return dao.findPopularBooks();
-    }
-
-    public List<Book> getBestSellers() {
-        return dao.findBestSellerBooks();
-    }
-
-    public List<Book> getRandomBooks() {
-        return dao.findRandomBooks();
-    }
-    public List<Book> getRecommandBook(Long currentId) {
-    	try {
-    		
-    	System.out.println("call python api ");
-        String url = "http://127.0.0.1:8848/recommend_by_id?book_id=" + currentId + "&top_n=5";
-
-        // API 호출
-        Book[] recommendedBooks = restTemplate.getForObject(url, Book[].class);
-        System.out.println(recommendedBooks);
-
-        return Arrays.asList(recommendedBooks);
-    	}catch(Exception e) {
-    		System.out.println(e);
-    		
-    	}finally {
-    		return getRandomBooks();
-    	}
-    }
-	
-	public PageList getPageList(int requestPage, String field, String keyword, String genre) {
-	    try {
-	        boolean isSearch = keyword != null && !keyword.trim().isEmpty();
-	        boolean isGenre = genre != null && !genre.trim().isEmpty();
-
-	        int totalCount;
-	        List<Book> list;
-	        int startnum = ((requestPage - 1) * 10) + 1;
-	        int endnum = requestPage * 10;
-
-	        if (isGenre) {
-	            totalCount = dao.countByGenre(genre);
-	            list = dao.findByGenre(genre, startnum, endnum);
-	        } else if (isSearch) {
-	            totalCount = dao.countBySearch(field, keyword);
-	            list = dao.searchByField(field, keyword, startnum, endnum);
-	        } else {
-	            totalCount = dao.count();
-	            list = dao.findAll(startnum, endnum);
-	        }
-
-	        PageList pageList = PageList.builder().totalCount(totalCount).pagePerCount(10).build();
-	        pageList.setCurrentPage(requestPage);
-	        int totalPage = (totalCount + 9) / 10;
-	        pageList.setTotalPage(totalPage);
-
-	        int startPage = ((requestPage - 1) / 5 * 5) + 1;
-	        int endPage = Math.min(startPage + 4, totalPage);
-	        pageList.setStartPage(startPage);
-	        pageList.setEndPage(endPage);
-	        pageList.setPre(requestPage > 5);
-	        pageList.setNext(endPage < totalPage);
-	        pageList.setList(list);
-
-	        return pageList;
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        return null;
-	    }
+		return dao.findPopularBooks();
 	}
 
+	public List<Book> getBestSellers() {
+		return dao.findBestSellerBooks();
+	}
 
-	
+	public List<Book> getRandomBooks() {
+		return dao.findRandomBooks();
+	}
+
+	public List<Book> getRecommandBook(Long currentId) {
+		try {
+
+			System.out.println("call python api ");
+			String url = "http://127.0.0.1:8848/recommend_by_id?book_id=" + currentId + "&top_n=5";
+
+			// API 호출
+			Book[] recommendedBooks = restTemplate.getForObject(url, Book[].class);
+			System.out.println(recommendedBooks);
+
+			return Arrays.asList(recommendedBooks);
+		} catch (Exception e) {
+			System.out.println(e);
+			return getRandomBooks();
+		}
+	}
+
+	public PageList getPageList(int requestPage, String field, String keyword, String genre) {
+		try {
+			boolean isSearch = keyword != null && !keyword.trim().isEmpty();
+			boolean isGenre = genre != null && !genre.trim().isEmpty();
+
+			int totalCount;
+			List<Book> list;
+			int startnum = ((requestPage - 1) * 10) + 1;
+			int endnum = requestPage * 10;
+
+			if (isGenre) {
+				totalCount = dao.countByGenre(genre);
+				list = dao.findByGenre(genre, startnum, endnum);
+			} else if (isSearch) {
+				totalCount = dao.countBySearch(field, keyword);
+				list = dao.searchByField(field, keyword, startnum, endnum);
+			} else {
+				totalCount = dao.count();
+				list = dao.findAll(startnum, endnum);
+			}
+
+			PageList pageList = PageList.builder().totalCount(totalCount).pagePerCount(10).build();
+			pageList.setCurrentPage(requestPage);
+			int totalPage = (totalCount + 9) / 10;
+			pageList.setTotalPage(totalPage);
+
+			int startPage = ((requestPage - 1) / 5 * 5) + 1;
+			int endPage = Math.min(startPage + 4, totalPage);
+			pageList.setStartPage(startPage);
+			pageList.setEndPage(endPage);
+			pageList.setPre(requestPage > 5);
+			pageList.setNext(endPage < totalPage);
+			pageList.setList(list);
+
+			return pageList;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 }
